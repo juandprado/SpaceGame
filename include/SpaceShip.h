8917012b6801fed431f75f942f59e62a97309939
juand <juandipra@hotmail.com>
2016-07-04 19:34:34 -0500

    Modificado weaponTimer

--- include/SpaceShip.h
+++ include/SpaceShip.h
@@ -16,10 +16,15 @@ public:
     virtual void Draw(sf::RenderWindow & render);
 
     static bool LoadImages();
+    float GetSpaceWidth();
+    float GetSpaceHeight();
+    sf::Vector2f GetSpacePosition();
 
 private:
 
     static sf::Image spaceShipImg;
+
+
 
     float rotationDir; // Direccion de rotacion de la nave
     float weaponTimer; // Variable encargada de almacenar el tiempo minimo entre cada disparo
